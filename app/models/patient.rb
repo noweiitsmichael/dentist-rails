17a124a38b4e72a83a9ac9a6e08ac9786b805a06
Dennis Qian <orangemoney52@gmail.com>
2015-04-15 00:43:30 -0700

    adding new patient scopes to patient model

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -4,4 +4,21 @@ class Patient < ActiveRecord::Base
   has_many :claims
 
   validates_uniqueness_of :od_uid, :scope => :practice_id
+
+  scope :new_since, lambda { |date| where("first_visit_date > ?", date) }
+  scope :new_since_today, lambda {
+    new_since(DateTime.now.beginning_of_day)
+  }
+  scope :new_since_yesterday, lambda {
+    new_since((DateTime.now - 1.day).beginning_of_day)
+  }
+  scope :new_since_last_week, lambda {
+    new_since((DateTime.now - 1.week).beginning_of_day)
+  }
+  scope :new_since_last_month, lambda {
+    new_since((DateTime.now - 1.month).beginning_of_day)
+  }
+  scope :new_between, lambda { |start_date, end_date|
+    where(:first_visit_date => start_date.beginning_of_day..end_date.end_of_day) 
+  }
 end
