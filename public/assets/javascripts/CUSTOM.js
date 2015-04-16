5bb6032186ecbfef337138e3b509896cfb267699
Michael Wei <michael.y.wei@gmail.com>
2015-04-15 23:51:52 -0700

    finished mocking up recommendations

--- public/assets/javascripts/CUSTOM.js
+++ public/assets/javascripts/CUSTOM.js
@@ -1,13 +1,22 @@
 $(function() {
 
   $(".recommendations-day").click( function() {
-    console.log("CLICK!");
     $(".arrow-down").hide();
-    console.log($(this).siblings());
     $(this).siblings().children().show();
     $(".rec-panel").slideUp("fast").delay( 300 );
     $(".rec-" + $(this).attr("day")).slideDown("fast");
   });
 
+  $(".recommendation input[type=checkbox]").click( function() {
+    var value = +($(this).parent().parent().find(".improvement-amount").html());
+    console.log(value);
+    if ( $(this).attr("checked") === "checked" ) {
 
+      console.log("Subtracting" + value);
+      value = value + +($(this).val());
+    } else {
+      value = value - +($(this).val());
+    }
+    $(this).parent().parent().find(".improvement-amount").html(value);
+  });
 });
